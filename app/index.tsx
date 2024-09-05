bbb9e1d6bcfa4336fcc2b6872b5b0f0c3ea8b80d
Colter Garrison <colter.garrison@gmail.com>
2024-09-05 13:38:49 -0700

    tamagui test and icon changes

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,49 @@
-import { View } from 'react-native';
-import { Button } from 'tamagui';
+import React from 'react';
+import {
+	Button,
+	H1,
+	H2,
+	H3,
+	H4,
+	H5,
+	H6,
+	Paragraph,
+	SizableText,
+	View,
+	XStack,
+	YStack,
+} from 'tamagui';
 
-const Page = () => {
+export default function Index() {
 	return (
 		<View>
-			<Button>BUTTON!</Button>
+			<YStack>
+				<H1>Heading 1</H1>
+				<H2>Heading 2</H2>
+				<H3>Heading 3</H3>
+				<H4>Heading 4</H4>
+				<H5>Heading 5</H5>
+				<H6>Heading 6</H6>
+				<Paragraph>Paragraph</Paragraph>
+				<SizableText size='$9'>SizableText</SizableText>
+				<Button alignSelf='center' size='$6'>
+					Large
+				</Button>
+			</YStack>
+			<YStack gap='$2' alignItems='center'>
+				<SizableText size='$3'>SizableText</SizableText>
+				<XStack gap='$2'>
+					<SizableText theme='alt1' size='$3'>
+						alt1
+					</SizableText>
+					<SizableText theme='alt2' size='$3'>
+						alt2
+					</SizableText>
+				</XStack>
+				<Paragraph size='$3' fontWeight='800'>
+					Paragraph
+				</Paragraph>
+			</YStack>
 		</View>
 	);
-};
-
-export default Page;
+}
