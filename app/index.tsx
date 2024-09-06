d50d9af75c094db19bce0f9f8a9c0e3bfd5a6c33
Colter Garrison <colter.garrison@gmail.com>
2024-09-06 10:28:42 -0700

    set BobsBurgers font to the default font family

--- app/index.tsx
+++ app/index.tsx
@@ -16,10 +16,10 @@ import {
 
 export default function Index() {
 	return (
-		<View>
+		<View bg={'#E4E4E5'}>
 			<YStack>
-				<H1 style={{ fontFamily: 'BobsBurgers' }}>Heading 1</H1>
-				<H2 style={{ fontFamily: 'BobsBurgers2' }}>Heading 2</H2>
+				<H1>Heading 1</H1>
+				<H2>Heading 2</H2>
 				<H3>Heading 3</H3>
 				<H4>Heading 4</H4>
 				<H5>Heading 5</H5>
