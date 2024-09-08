00ab44ba8b4dbb7ff72f5ed0bd7e079f0eb3b3b4
Colter Garrison <colter.garrison@gmail.com>
2024-09-08 12:15:54 -0700

    update characters styling

--- hooks/fetchCharacters.ts
+++ hooks/fetchCharacters.ts
@@ -1,7 +1,7 @@
 export const getCharacters = async () => {
 	try {
 		const response = await fetch(
-			'https://bobsburgers-api.herokuapp.com/characters/'
+			'https://bobsburgers-api.herokuapp.com/characters?sortBy=name&OrderBy=asc'
 		);
 		const data = await response.json();
 		return data;
