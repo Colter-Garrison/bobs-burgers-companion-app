2dd47b9afb714e3dcc969c90cadf6c8b05478a06
Colter Garrison <colter.garrison@gmail.com>
2024-09-08 16:33:11 -0700

    make characters and episodes clickable

--- app/characters.tsx
+++ app/characters.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Image } from 'react-native';
+import { Image, Linking, Pressable } from 'react-native';
 import { H1, ScrollView, SizableText, XStack, YStack } from 'tamagui';
 import { getCharacters } from '../hooks/fetchCharacters';
 
@@ -27,6 +27,9 @@ export default function Characters() {
 
 	const [characters, setCharacters] = useState<Character[]>([]);
 	const [loading, setLoading] = useState(true);
+	const handlePress = (character: Character) => {
+		Linking.openURL(character.wikiUrl);
+	};
 
 	useEffect(() => {
 		const fetchData = async () => {
@@ -71,67 +74,74 @@ export default function Characters() {
 				{characters.length > 0 ? (
 					characters.map((character) => {
 						return (
-							<XStack
+							<Pressable
 								key={character.id}
-								gap='$2'
-								backgroundColor={'#F8DF24'}
-								borderWidth={4}
-								borderColor={'#E8242F'}
-								borderRadius={8}
-								padding='$2'
-								alignItems='center'
+								onPress={() => handlePress(character)}
 							>
-								{character.image ? (
-									<Image
-										source={{ width: 100, height: 100, uri: character.image }}
-										width={100}
-										height={100}
-										resizeMode='contain'
-									/>
-								) : null}
-								<YStack maxWidth='70%'>
-									<SizableText color='#E8242F'>
-										Name: {character.name}
-									</SizableText>
-									<SizableText>
-										{character.relatives.length > 0 ? (
-											<SizableText color='#E8242F'>
-												Relatives:{' '}
-												{character.relatives
-													.map((relative) => relative.name)
-													.join(', ')}
-											</SizableText>
-										) : (
-											<SizableText color='#E8242F'>Relatives: None</SizableText>
-										)}
-									</SizableText>
-									<SizableText>
-										{character.occupation ? (
-											<SizableText color='#E8242F'>
-												Occupation: {character.occupation}
-											</SizableText>
-										) : (
-											<SizableText color='#E8242F'>
-												Occupation: None
-											</SizableText>
-										)}
-									</SizableText>
-									<SizableText color='#E8242F'>
-										First Episode: {character.firstEpisode}
-									</SizableText>
-									<SizableText>
-										{character.voicedBy ? (
-											<SizableText color='#E8242F'>
-												Voiced By: {character.voicedBy}
-											</SizableText>
-										) : (
-											<SizableText color='#E8242F'>
-												Voiced By: Unknown
-											</SizableText>
-										)}
-									</SizableText>
-								</YStack>
-							</XStack>
+								<XStack
+									key={character.id}
+									gap='$2'
+									backgroundColor={'#F8DF24'}
+									borderWidth={4}
+									borderColor={'#E8242F'}
+									borderRadius={8}
+									padding='$2'
+									alignItems='center'
+								>
+									{character.image ? (
+										<Image
+											source={{ width: 100, height: 100, uri: character.image }}
+											width={100}
+											height={100}
+											resizeMode='contain'
+										/>
+									) : null}
+									<YStack maxWidth='70%'>
+										<SizableText color='#E8242F'>
+											Name: {character.name}
+										</SizableText>
+										<SizableText>
+											{character.relatives.length > 0 ? (
+												<SizableText color='#E8242F'>
+													Relatives:{' '}
+													{character.relatives
+														.map((relative) => relative.name)
+														.join(', ')}
+												</SizableText>
+											) : (
+												<SizableText color='#E8242F'>
+													Relatives: None
+												</SizableText>
+											)}
+										</SizableText>
+										<SizableText>
+											{character.occupation ? (
+												<SizableText color='#E8242F'>
+													Occupation: {character.occupation}
+												</SizableText>
+											) : (
+												<SizableText color='#E8242F'>
+													Occupation: None
+												</SizableText>
+											)}
+										</SizableText>
+										<SizableText color='#E8242F'>
+											First Episode: {character.firstEpisode}
+										</SizableText>
+										<SizableText>
+											{character.voicedBy ? (
+												<SizableText color='#E8242F'>
+													Voiced By: {character.voicedBy}
+												</SizableText>
+											) : (
+												<SizableText color='#E8242F'>
+													Voiced By: Unknown
+												</SizableText>
+											)}
+										</SizableText>
+									</YStack>
+								</XStack>
+							</Pressable>
 						);
 					})
 				) : (
